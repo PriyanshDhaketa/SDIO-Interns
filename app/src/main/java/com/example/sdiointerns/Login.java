5058da723cf7d67ba95fb9bfb573ece270e47563
Vipul <Vipulsuthar135@gmail.com>
2021-02-25 14:36:29 +0530

    Login Page Added

--- app/src/main/java/com/example/sdiointerns/Login.java
+++ app/src/main/java/com/example/sdiointerns/Login.java
@@ -2,15 +2,31 @@ package com.example.sdiointerns;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button btn_gotoregister,signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        btn_gotoregister = findViewById(R.id.btn_go_to_register);
+        signup = findViewById(R.id.btn_login);
+
+        btn_gotoregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this , Registration.class);
+                startActivity(intent);
+            }
+        });
     }
 }
